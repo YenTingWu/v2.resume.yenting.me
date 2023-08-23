6c530fd5ce007909972be5a04829086d4e495082
ytw <a9600125a@gmail.com>
2023-08-23 17:45:38 +0800

    feat: link to the pdf version

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className="w-full mt-10 flex justify-center font-PT">
       <span className="mx-auto px-5 py-5 border-t-2 border-zinc-300">
-        <a className="hover:underline" href="resume.pdf">
+        <a className="hover:underline" href="/resume.pdf">
           PDF Version
         </a>
       </span>
