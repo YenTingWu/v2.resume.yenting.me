a57caa0f753593c7f8624dc7f9bb142f3b64aca6
ytw <a9600125a@gmail.com>
2023-08-23 13:39:16 +0800

    feat: complete stacks section

--- src/components/stacks.tsx
+++ src/components/stacks.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
+import data from "../data.json";
 
 export const Stacks = () => {
+  const { stacks } = data;
+
   return (
     <div className="card-container">
       <div className="p-2">
@@ -9,32 +12,18 @@ export const Stacks = () => {
         </h4>
         <div className="card-content tracking-wider lg:leading-6 leading-2">
           <div className="grid grid-cols-6 lg:max-w-full max-w-sm gap-x-5 gap-y-2">
-            <span className="font-bold lg:col-span-1 col-span-2 text-right">
-              Front End
-            </span>
-            <span className="lg:col-span-5 col-span-4">React, Svelte</span>
-            <span className="font-bold lg:col-span-1 col-span-2 text-right">
-              Back End
-            </span>
-            <span className="lg:col-span-5 col-span-4">
-              Node, Gin, PostgresQL, Serverless, Docker
-            </span>
-            <span className="font-bold lg:col-span-1 col-span-2 text-right">
-              Mobile
-            </span>
-            <span className="lg:col-span-5 col-span-4">Expo, React Native</span>
-            <span className="font-bold lg:col-span-1 col-span-2 text-right">
-              DevOps
-            </span>
-            <span className="lg:col-span-5 col-span-4">
-              Firebase, Droplets, Heroku
-            </span>
-            <span className="font-bold lg:col-span-1 col-span-2 text-right">
-              Language
-            </span>
-            <span className="lg:col-span-5 col-span-4">
-              Typescript, Javascript, Go
-            </span>
+            {Object.entries(stacks).map(([key, value]) => (
+              <React.Fragment key={key}>
+                <span className="font-bold lg:col-span-1 col-span-2 text-right">
+                  {key}
+                </span>
+                <span className="lg:col-span-5 col-span-4">
+                  {value.map((v, i) =>
+                    i === value.length - 1 ? `${v}` : `${v}, `
+                  )}
+                </span>
+              </React.Fragment>
+            ))}
           </div>
         </div>
       </div>
